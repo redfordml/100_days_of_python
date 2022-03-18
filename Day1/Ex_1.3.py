f72e483878289082e2830b9eb320e6c94cdc9daa
Redford Ferreras <redfordml@gmail.com>
2022-03-17 21:10:01 -0400

    band name project

--- Day1/Ex_1.3.py
+++ Day1/Ex_1.3.py
@@ -1,10 +1,12 @@
 ## Write a program that prints the number of characters in a user's name.
 
-print(len(input("Whats your name? ")))
+#print(len(input("Whats your name? ")))
 
 
 
-
+name = input ("What is your name?")
+lenght = len(name)
+print(lenght)
 
 
 
