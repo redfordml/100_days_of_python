3c3603f26cea7efef0d960e2aa5f07ed531361d3
Redford Ferreras <redfordml@gmail.com>
2022-03-20 14:36:18 -0400

    four-digit-number

---
+++ day2/Ex_2.1.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+four_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+
+#Write a program that adds the digits in a 4 digit number. e.g. if the input was 3540, then the output should be 3 + 5 + 4 + 0 = 12
+#Warning. Do not change the code on lines 1-3. Your program should work for different inputs. e.g. any four-digit number.
+
+
+
+
+print (type(four_digit_number))
+
+num1 = (four_digit_number [0])
+num2 = (four_digit_number [1])
+num3 = (four_digit_number [2])
+num4 = (four_digit_number [3])
+
+print ( int(num1) + int (num2) + int (num3) + int (num4) )
+
+
