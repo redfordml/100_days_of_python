407d2a89b7cbfe35df0c541f7bf26b1fd1bcb38a
Redford Ferreras <redfordml@gmail.com>
2022-04-07 00:31:08 -0400

    av

--- day2/data_automation.py
+++ day2/data_automation.py
@@ -1,29 +1,58 @@
-import numpy as np
+import csv
 
 #read and count documment
 
 #opening file
 
-file = open(r"C:\Users\Redford\Documents\Proyecto automatizacion\data.txt")
+file = open (r"C:\Users\Redford\Documents\Proyecto automatizacion\data1.txt") #open file
 counter = 0
+total_lines = len(file.readlines()) #count lines
+#print('Total lines:', x)
 
 #reading from file
-
 content = file.read()
-colist  = content.split ("\n")
+colist  = content.split ()
+print(colist)
+
+
+
 
 
 for i in colist:
     if i:
         counter += 1
-        print(colist)
+
+print(counter) #cuenta cuatas lineas tine el documento txt
+            
+
+            
+lines = open (r"C:\Users\Redford\Documents\Proyecto automatizacion\data1.txt")
+arr = []
+with open('readme.txt', 'w') as f:
+    for line in lines:
+        #f.write(line)
+        #f.write('%d')
+        #f.write(line)
+        arr.append(line)
+        f.write([line])
+        #f.write(line + ' ')
+        
+        print ([arr])
+
+        
+        
 
 
-print(counter)
+
 
         
 #locating and reading file
 
 
-with open (r"C:\Users\Redford\Documents\Proyecto automatizacion\data.txt") as file:
-    lines = [line.strip() for line in file]
+#with open (r"C:\Users\Redford\Documents\Proyecto automatizacion\data.txt") as file:
+# lines = [line.strip() for line in file]
+
+
+
+
+
